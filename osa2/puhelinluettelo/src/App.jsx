894a4a5cfa4a9724d239bb5019a9d70ff7a5966d
Sane <heikkinensanteri@gmail.com>
2024-03-28 19:42:14 +0200

    add task 2.8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,26 +2,33 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123-1244'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
    if (!personSame()) {
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
       }
     }
-  
 
   const handlePersonChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   const personSame = () => {
@@ -37,21 +44,20 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} 
-                onChange={handlePersonChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div>name:<br></br> <input value={newName} onChange={handlePersonChange}/></div>
+        <div>number:<br></br> <input value={newNumber} onChange={handleNumberChange}/></div>
+        <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) =>
+        {persons.map(person => { 
+          console.log(person)
+          return (
           <li key={person.name}>
-            {person.name}
+            {person.name} {person.number}
           </li>
-          )}
+          );
+        })}
       </ul>
 
     </div>
