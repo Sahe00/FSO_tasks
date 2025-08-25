fd0050e0627b3b79b403b4193c39ca5a639014a5
Sahe00 <71127023+Sahe00@users.noreply.github.com>
2025-08-25 19:49:56 +0300

    add tasks 3.7-3.8

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 let persons = [
@@ -25,6 +26,12 @@ let persons = [
 ]
 
 app.use(express.json())
+
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/info', (request, response) => {
     const count = persons.length;
