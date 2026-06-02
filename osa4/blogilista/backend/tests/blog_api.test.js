05ec84d11d0626681e4c59905bded6da7650c8ac
Santeri Heikkinen <saheikki20@student.oulu.fi>
2026-06-02 21:02:35 +0300

    add task 4.8

---
+++ osa4/blogilista/backend/tests/blog_api.test.js
@@ -0,0 +1,50 @@
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+const initialBlogs = [
+    {
+        title: 'Testauksen salat',
+        author: 'Teuvo Testimies',
+        url: 'http://testi.com',
+        likes: 12,
+    },
+  {
+        title: 'Rauta tottelee',
+        author: 'Teemu Testomies',
+        url: 'http://testo.com',
+        likes: 9,
+  },
+]
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+})
+
+// Checking blog is returned as JSON
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+// Checking the number of blogs is correct 
+test('all blogs are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})
